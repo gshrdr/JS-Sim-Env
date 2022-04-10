7a2df5cc2e57e367cbd810ed100468a839dfcfa2
gshrdr <shrader.gavin@gmail.com>
2022-04-10 16:40:47 -0400

    Cleaned up style, html, and javascript code

--- scripts/resize.js
+++ scripts/resize.js
@@ -1,43 +1,4 @@
-/*
-(function() {
-  var
-    // Obtain a reference to the canvas element using its id.
-    htmlCanvas = document.
-    // Obtain a graphics context on the canvas element for drawing.
-    context = htmlCanvas.getContext('2d');
-
-  // Start listening to resize events and draw canvas.
-  initialize();
-
-  function initialize() {
-    // Register an event listener to call the resizeCanvas() function
-    // each time the window is resized.
-    window.addEventListener('resize', resizeCanvas, false);
-    // Draw canvas border for the first time.
-    resizeCanvas();
-  }
-
-  // Display custom canvas. In this case it's a blue, 5 pixel
-  // border that resizes along with the browser window.
-  function redraw() {
-
-    context.strokeStyle = 'blue';
-    context.lineWidth = '5';
-    context.strokeRect(0, 0, htmlCanvas.width, htmlCanvas.height);
-  }
-
-  // Runs each time the DOM window resize event fires.
-  // Resets the canvas dimensions to match window,
-  // then draws the new borders accordingly.
-  function resizeCanvas() {
-    redraw();
-  }
-})();
-*/
-
-
 // Handle Canvas Draw Call
-// Tutorial: https://thewebdev.info/2021/03/01/how-to-resize-html5-canvas-to-fit-the-window/
 const draw = (canvas) => {
   const ctx = canvas.getContext("2d");
   ctx.strokeStyle = 'blue';
@@ -60,7 +21,7 @@ window.onload = function(){
        canvas.height = container.clientHeight
        console.log("DRAW AGAIN: Canvas Width: " + canvas.width + " Canvas Height: " + canvas.height + " Container Width: " + container.clientWidth + " Container Height: " + container.clientHeight);
        draw(canvas)
-   }, 1000);
+   }, 500);
 };
 
 // Listen For Window Resize
