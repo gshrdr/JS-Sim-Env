3b5a8f7aef3e9e00b034ebc680717a911c3c6d5d
gshrdr <shrader.gavin@gmail.com>
2022-04-14 19:37:22 -0400

    Created utilities file for things like randomRGBColor(), etc.

--- scripts/mainGameLoop.js
+++ scripts/mainGameLoop.js
@@ -172,16 +172,6 @@ function endFPS() {
   fpsElement.innerHTML = "FPS: --";
 }
 
-// Get a random RGB color
-// Source: https://stackoverflow.com/questions/23095637/how-do-you-get-random-rgb-in-javascript
-function getRandomRgb() {
-  var num = Math.round(0xffffff * Math.random());
-  var r = num >> 16;
-  var g = num >> 8 & 255;
-  var b = num & 255;
-  return 'rgb(' + r + ', ' + g + ', ' + b + ')';
-}
-
 /*
  * Handling Resizing - Tile Map Layer & Entities Layer
  */
